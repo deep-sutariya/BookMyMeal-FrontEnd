d772d5e193ee316ce5419609ba7935b436774f8d
dhruv9596 <dhruvlodaliya9596@gmail.com>
2024-01-06 12:14:41 +0530

    react-charjs-2

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -5,11 +5,14 @@ import { useContext } from "react";
 const DashBoard = () => {
     const { loginuser } = useContext(LoginDetails);
     return (
-      <div className=' my-20'>
+      <div className=" my-20">
         {loginuser ? (
-          <div className=' flex flex-col gap-y-10 mt-10'>
-            <DashboardChart charttype="Line" />
+          <div className=" flex flex-col gap-y-10 mt-10">
+            <DashboardChart charttype="Bar" />
             <DashboardChart charttype="Doughnut" />
+            <DashboardChart charttype="Line" />
+            <DashboardChart charttype="Pie" />
+            <DashboardChart charttype="Doughnut-age" />
           </div>
         ) : (
           <></>
--- src/components/DashboardChart.jsx
+++ src/components/DashboardChart.jsx
@@ -4,7 +4,7 @@ import BounceLoader from "react-spinners/BounceLoader";
 import { LoginDetails } from "../contex/Logincontex";
 import { useContext } from "react";
 import { Chart } from "chart.js/auto";
-import { Doughnut, Line } from "react-chartjs-2";
+import { Doughnut, Line ,Bar , Pie} from "react-chartjs-2";
 
 
 const DashboardChart = ({ charttype }) => {
@@ -25,9 +25,18 @@ const DashboardChart = ({ charttype }) => {
     setLoading(false);
   };
   let burger = 0, pizza = 0;
+  let surat = 0,
+    bhavnagar = 0,
+    junagadh = 0,
+    vadodara = 0,
+    ahemdabad = 0,
+    kutchh = 0,
+    other = 0;
+  let young = 0 , middle = 0 , senior = 0;
+  let male= 0 , female = 0;
   const length = 12; // Set the desired length of the array
   const totalOrders = new Array(length).fill(0);
-  for (let i = 0; i < morder?.length; i++) {
+  for (let i = 0; i < morder?.length; i++) {  
     var type = morder[i].type;
     console.log('Type ', type)
     if (type === "pizza") {
@@ -36,6 +45,25 @@ const DashboardChart = ({ charttype }) => {
     else {
       burger++;
     }
+    var area = morder[i].city;
+    if( area == "Surat"){ surat++;}
+    else if( area =="Bhavnagar"){bhavnagar++;}
+    else if( area == "Vadodara" ){vadodara++;}
+    else if( area == "Kutchh"){kutchh++;}
+    else if( area == "Junagadh"){junagadh++;}
+    else{
+      other++;
+    }
+    let age = parseInt(morder[i].uage, 10);
+    if( age >= 0 && age <= 18 ){ young++; }
+    else if( age >= 18 && age <=40 ){ middle++ ;}
+    else{ senior++ ;}
+
+    let gender = morder[i].ugender;
+    if( gender == "male"){ male++; }
+    else{ female++;}
+    console.log('uage' , age);
+    console.log("City ", surat, bhavnagar , vadodara , kutchh , junagadh , other);
     console.log('Pizza & Burger ', pizza, burger);
     let m = parseInt(morder[i].month, 10);
     if (m >= 1 && m <= 12) {
@@ -77,7 +105,7 @@ const DashboardChart = ({ charttype }) => {
         label: "Month wise Orders",
         fill: false,
         lineTension: 0.2,
-        backgroundColor: "rgba(75,192,192,0.4)",
+        backgroundColor: "#800080",
         borderColor: "black",
         borderCapStyle: "butt",
         borderDash: [],
@@ -106,25 +134,77 @@ const DashboardChart = ({ charttype }) => {
       },
     ],
   };
+  const doughnutAgeState = {
+    labels: ["Male", "Female"],
+    datasets: [
+      {
+        backgroundColor: ["#00A6B4", "#6800B4"],
+        hoverBackgroundColor: ["#4B5000", "#35014F"],
+        data: [male, female],
+      },
+    ],
+  };
+  const pieState = {
+    labels: [
+      "Surat",
+      "Vadodara",
+      "Kutchh",
+      "Bhavnagar",
+      "Junagadh",
+      "Ahemdabad",
+      "Others",
+    ],
+    datasets: [
+      {
+        backgroundColor: [
+          "#FF0000",
+          "#00FF00",
+          "#0000FF",
+          "#FFFF00",
+          "#800080",
+          "#FFA500",
+          "#008080",
+        ],
+        hoverBackgroundColor: ["#4B5000", "#35014F"],
+        data: [surat, vadodara, kutchh, bhavnagar, junagadh, ahemdabad, other],
+      },
+    ],
+  };
+
+  const lineState = {
+    labels: ["0-18", "18-40", "40-70"],
+    datasets: [
+      {
+        label: "Age",
+        data: [young, middle, senior],
+        borderColor: "#0000FF",
+        borderWidth: 1,
+        fill: false,
+      },
+    ],
+  };
+
   useEffect(() => {
     getData();
   }, []);
   return (
     <div className="mx-auto w-3/4">
-      {charttype === "Line" ? (
+      {charttype === "Bar" ? (
         <div className="flex items-center justify-center gap-x-10 flex-row h-1/4 w-full">
           <div className="w-1/2 flex justify-center py-6 items-center h-[400px] bg-slate-50 p-4">
-            <Line
+            <Bar
               data={chartData}
               options={options}
               className="w-5/6 h-[200px]"
             />
           </div>
           <div className="w-1/2 flex items-center justify-center">
-            <div className=" font-bold text-xl text-red-400">Monthwise Orders</div>
+            <div className=" font-bold text-xl text-red-400">
+              Monthwise Orders
+            </div>
           </div>
         </div>
-      ) : (
+      ) : charttype == "Doughnut" ? (
         <div className="flex items-center justify-center gap-x-10 flex-row h-1/4 w-full">
           <div className="w-1/2 flex items-center justify-center">
             <div className=" font-bold text-xl text-red-400">Items</div>
@@ -133,92 +213,42 @@ const DashboardChart = ({ charttype }) => {
             <Doughnut data={doughnutState} className="w-5/6 h-[100px]" />
           </div>
         </div>
+      ) : charttype == "Line" ? (
+        <div className="flex items-center justify-center gap-x-10 flex-row h-1/4 w-full">
+          <div className="w-1/2 flex justify-center py-6 items-center h-[400px] bg-slate-50 p-4">
+            <Line
+              data={lineState}
+              options={options}
+              className="w-5/6 h-[200px]"
+            />
+          </div>
+          <div className="w-1/2 flex items-center justify-center">
+            <div className=" font-bold text-xl text-red-400">
+              Agewise Orders
+            </div>
+          </div>
+        </div>
+      ) : charttype == "Pie" ? (
+        <div className="flex items-center justify-center gap-x-10 flex-row h-1/4 w-full">
+          <div className="w-1/2 flex items-center justify-center">
+            <div className=" font-bold text-xl text-red-400">City</div>
+          </div>
+          <div className="w-1/2 flex justify-center py-6 items-center h-[400px] bg-slate-50 p-4">
+            <Pie data={pieState} className="w-5/6 h-[100px]" />
+          </div>
+        </div>
+      ) :(
+        <div className="flex items-center justify-center gap-x-10 flex-row h-1/4 w-full">
+          <div className="w-1/2 flex justify-center py-6 items-center h-[400px] bg-slate-50 p-4">
+            <Doughnut data={doughnutAgeState} className="w-5/6 h-[100px]" />
+          </div>
+          <div className="w-1/2 flex items-center justify-center">
+            <div className=" font-bold text-xl text-red-400">Genderwise Orders</div>
+          </div>
+        </div>
       )}
     </div>
   );
 }
-// const DashboardChart = ({ charttype }) => {
-//   let [loading, setLoading] = useState(true);
-//   const [ morder , setMorder ] = useState();
-//   const { loginuser } = useContext(LoginDetails);
-//   const getData = async () => {
-//     setLoading(true);
-//     const data = await axios.post(
-//       `${process.env.REACT_APP_HOST_IP}/getmedorder`,
-//       {
-//         memail: loginuser?.memail,
-//       }
-//     );
-//     const d = data?.data?.data;
-//     setMorder(d);
-//     console.log("Data ", d);
-//     setLoading(false);
-//     console.log('Mo ' , morder);
-//   };
-//   let young = [];
-//   let middle = [];
-//   let senior = [];
-//   for (let i = 0; i < morder?.length; i++) {
-//     if (morder[i].uage > 0 && morder[i].uage <= 18) {
-//       young = [...young, morder[i].uage];
-//     } else if (morder[i].uage > 18 && morder[i].uage <= 50) {
-//       middle = [...middle, morder[i].uage];
-//     } else {
-//       senior = [...senior, morder[i].uage];
-//     }
-//   }
-//   console.log("Young ", young);
-//   console.log("Middle ", middle);
-//   console.log("Senior ", senior);
-//   useEffect(() => {
-//     getData();
-//   }, []);
-//    const options = {
-//   responsive: true,
-//   plugins: {
-//     legend: {
-//       position: 'top',
-//     },
-//     title: {
-//       display: true,
-//       text: 'Chart.js Line Chart',
-//     },
-//   },
-// };
-
-//   const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-//   const data = {
-//   labels,
-//   datasets: [
-//     {
-//       label: 'Dataset 1',
-//       data: middle,
-//       borderColor: 'rgb(255, 99, 132)',
-//       backgroundColor: 'rgba(255, 99, 132, 0.5)',
-//     },
-//     {
-//       label: 'Dataset 2',
-//       data: young,
-//       borderColor: 'rgb(53, 162, 235)',
-//       backgroundColor: 'rgba(53, 162, 235, 0.5)',
-//     },
-//   ],
-//   };
-//   return (
-//     <div className="mx-auto">
-//       {charttype === "Line" ? (
-//         <div className="flex items-center justify-center gap-x-10 flex-row h-full w-full">
-//           <div className="w-full h-full bg-slate-200">
-//             <Line data={data} className="w-1/2"/>
-//           </div>
-//           <div className="w-1/2">DESCRIPTIOON</div>
-//         </div>
-//       ) : (
-//         <Doughnut data={data} />
-//       )}
-//     </div>
-//   );
-// };
 
 export default DashboardChart;
